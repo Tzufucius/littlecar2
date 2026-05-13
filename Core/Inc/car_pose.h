eef9fb9c03621e3ba0cb5e9b6ec69a894d026be5
Tzufucius <952105672@qq.com>
2026-05-13 11:20:10 +0800

    1

--- Core/Inc/car_pose.h
+++ Core/Inc/car_pose.h
@@ -2,19 +2,33 @@
 #define __CAR_POSE_H__
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include "sensor_ops.h"
 #include "sensor_wit.h"
 
-extern const volatile WIT_Data_t *carpose_imu;
-extern const volatile OPS_Pose_t *carpose_ops;
+    extern const volatile WIT_Data_t *carpose_imu;
+    extern const volatile OPS_Pose_t *carpose_ops;
 
-void CarPose_Init(void);
+    void CarPose_Init(void);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
+
+// 示例用法
+// #include "car_pose.h"
+
+// if ((carpose_ops != NULL) && (carpose_ops->valid != 0U))
+// {
+//     float x = carpose_ops->pos_x_mm;
+// }
+
+// if ((carpose_imu != NULL) && (carpose_imu->angle_deg.valid != 0U))
+// {
+//     float yaw = carpose_imu->angle_deg.z;
+// }
