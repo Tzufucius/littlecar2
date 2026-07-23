bdc5a3602663d757246c67de9acc5ad9530ee543
Tzufucius <952105672@qq.com>
2026-07-23 15:44:19 +0800

    feat: 6色物料yolo数据集

--- jetson/scripts/test_yolo.py
+++ jetson/scripts/test_yolo.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import sys
-
+import numpy as np
+import cv2
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(PROJECT_ROOT / "src"))
 
-from vision.yolo_detector import YoloDetector
+from vision import load_yolo_model, detect_yolo
 
 
 IMAGE_PATH = PROJECT_ROOT / "assets" / "sim_train_00025.jpg"
@@ -15,12 +16,9 @@ CONF_THRES = 0.5
 
 
 def main() -> None:
-    detector = YoloDetector(
-        model_path=MODEL_PATH,
-        conf_thres=CONF_THRES,
-        device=DEVICE,
-    )
-    detections = detector.detect(IMAGE_PATH)
+    detector = load_yolo_model(model_path=MODEL_PATH)
+    np_img = cv2.imread(str(IMAGE_PATH))
+    detections = detect_yolo(np_img, detector, device=DEVICE, conf_thres=CONF_THRES)
     for det in detections:
         print(det)
 
